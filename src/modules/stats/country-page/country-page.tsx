25e2546384440ae73992094acd599df5627abffd
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-01-05 16:48:10 +0200

    Added calendar in country-page

--- src/modules/stats/country-page/country-page.tsx
+++ src/modules/stats/country-page/country-page.tsx
@@ -1,15 +1,22 @@
-import React, {useEffect} from 'react';
-import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import 'react-calendar/dist/Calendar.css';
+import {useParams} from 'react-router-dom';
+import Calendar from 'react-calendar';
 
 export const CountryPage = () => {
     const {country} = useParams();
+    const [value, onChange] = useState(new Date());
 
     useEffect(() => {
-        console.log(`render ${country}`)
-    }, [])
+        console.log(value.toDateString() === new Date('2022-01-05T00:00:00Z').toDateString());
+    }, [value])
     return (
         <div>
             <h1>{country}</h1>
+            <Calendar
+                onChange={onChange}
+                value={value}
+            />
         </div>
     );
 };
